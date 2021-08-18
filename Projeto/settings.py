dd58df9ea34e0ac9d80bccb9903743033e49f1bb
hermelio <hermelio.ifma@gmail.com>
2021-08-18 08:41:52 -0300

    Versao2.0

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportifolio.urls'
+ROOT_URLCONF = 'projeto.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportifolio.wsgi.application'
+WSGI_APPLICATION = 'projeto.wsgi.application'
 
 
 # Database
